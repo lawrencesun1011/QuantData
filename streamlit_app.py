5d23f70e0dfb602c6a34a888144987bc393815c3
SunMac <sun@macbook.com>
2025-10-30 09:43:02 +0800

    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ st.title("量化策略实盘监控")
 DATA_URL = "https://gist.githubusercontent.com/lawrencesun1011/a896403c442e4f8d13cb6ecb9e331b48/raw/data.csv"
 
 # 加载数据的函数
-@st.cache_data
+@st.cache_data(ttl=1200)
 
 def calculate_drawdown(equity_series):
     # 计算累计最大值
